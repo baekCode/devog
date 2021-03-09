65662f9ce7c46f70f33b256b2a8cd1437e25aa49
baekCode <44539224+baekCode@users.noreply.github.com>
2021-03-09 23:33:29 +0900

    feat : koa middleware 개념

--- server/src/index.js
+++ server/src/index.js
@@ -2,9 +2,25 @@ const Koa = require('koa');
 
 const app = new Koa();
 
-app.use(cts => {
-  cts.body = 'Hello World';
+app.use((ctx, next) => {
+  console.log(ctx.url);
+  console.log(1);
+  if (ctx.query.authorized !== '1') {
+    ctx.status = 401;
+    return;
+  }
+  next();
 });
+
+app.use((ctx, next) => {
+  console.log(2);
+  next();
+});
+
+app.use(ctx => {
+  ctx.body = 'Hello World';
+});
+
 
 app.listen(4000, () => {
   console.log('Listening to port : 4000');
