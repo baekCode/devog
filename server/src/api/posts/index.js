c4dda84ba5dcbc0dafa8f53f61f5b5dabc092649
baekCode <44539224+baekCode@users.noreply.github.com>
2021-03-11 23:01:02 +0900

    feat : post router 컨트롤러 적용

--- server/src/api/posts/index.js
+++ server/src/api/posts/index.js
@@ -1,5 +1,6 @@
 const Router = require('koa-router');
 const posts = new Router();
+const postsCtrl = require('./posts.ctrl');
 
 const printInfo = ctx => {
   const {method, path, params} = ctx;
@@ -10,11 +11,11 @@ const printInfo = ctx => {
   };
 };
 
-posts.get('/', printInfo);
-posts.post('/', printInfo);
-posts.get('/:id', printInfo);
-posts.delete('/:id', printInfo);
-posts.put('/:id', printInfo);
-posts.patch('/:id', printInfo);
+posts.get('/', postsCtrl.list);
+posts.post('/', postsCtrl.write);
+posts.get('/:id', postsCtrl.read);
+posts.delete('/:id', postsCtrl.remove);
+posts.put('/:id', postsCtrl.replace);
+posts.patch('/:id', postsCtrl.update);
 
 module.exports = posts;
