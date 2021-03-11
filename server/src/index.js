05ee4f5c10b06235c28d853c907db201d7b01a4a
baekCode <44539224+baekCode@users.noreply.github.com>
2021-03-11 23:08:38 +0900

    add : bodyparser

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,17 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
+const api = require('./api');
 
 const app = new Koa();
 const router = new Router();
 
-const api = require('./api');
-
 // 라우터 설정
 router.use('/api', api.routes())
+
+// 라우터 적용 전에 bodyparser 적용
+app.use(bodyParser());
 
 // use : app router
 app.use(router.routes()).use(router.allowedMethods());
