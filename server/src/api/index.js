043a3a45d71d4455584cd7c6a213dbe233f51e6f
baekCode <44539224+baekCode@users.noreply.github.com>
2021-03-11 22:36:09 +0900

    feat : skeleton posts api 구현

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,8 +1,8 @@
 const Router = require('koa-router');
 const api = new Router();
 
-api.get('/test', ctx => {
-  ctx.body = 'test success';
-})
+const posts = require('./posts');
+
+api.use('/posts', posts.routes());
 
 module.exports = api;
