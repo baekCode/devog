e38870cae819233469330d8230c0bb24d66d3321
baekCode <baekcode@gmail.com>
2021-03-13 21:30:28 +0900

    feat : dotenv 환경변수

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+
+const {PORT} = process.env;
 
 const api = require('./api');
 
 const app = new Koa();
 const router = new Router();
+const port = PORT || 4000;
 
 // 라우터 설정
 router.use('/api', api.routes())
@@ -16,6 +20,6 @@ app.use(bodyParser());
 // use : app router
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('Listening to port : 4000');
+app.listen(port, () => {
+  console.log('Listening to port : %d', port);
 });
