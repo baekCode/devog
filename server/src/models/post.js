73faf4405a10ebfab24965fe1f3e4eb859293efd
baekCode <baekcode@gmail.com>
2021-03-13 22:23:03 +0900

    feat : mongo Schema 를 이용한 POST Schema 정의

---
+++ server/src/models/post.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const {Schema} = mongoose;
+
+/**
+ * POST SCHEMA
+ * title {string} 제목
+ * body {string} 제목
+ * tags {string[]} 태그 목록
+ * publishedDate {date} 작성 날짜
+ * */
+
+const PostSchema = new Schema({
+  title        : String,
+  body         : String,
+  tags         : [String],
+  publishedDate: {
+    type   : Date,
+    default: Date.now // 현재 날짜를 기본값
+  }
+});
