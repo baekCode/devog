f1f676b8820f4e7249fe2aecc8be3ae7c1d93ba4
baekCode <baekcode@gmail.com>
2021-03-13 21:47:11 +0900

    feat : mongoose를 이용한 데이터베이스 연결

--- server/src/index.js
+++ server/src/index.js
@@ -2,17 +2,28 @@ require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
-
-const {PORT} = process.env;
+const mongoose = require('mongoose');
 
 const api = require('./api');
+
+const {PORT, MONGO_URI} = process.env;
+
+mongoose
+  .connect(MONGO_URI, {useNewUrlParser: true, useFindAndModify: false})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(e => {
+    console.error(e);
+  });
+
 
 const app = new Koa();
 const router = new Router();
 const port = PORT || 4000;
 
 // 라우터 설정
-router.use('/api', api.routes())
+router.use('/api', api.routes());
 
 // 라우터 적용 전에 bodyparser 적용
 app.use(bodyParser());
