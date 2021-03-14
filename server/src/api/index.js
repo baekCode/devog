0f9d473d7b25436d444ceb91ba046d0e94bed5e8
baekCode <44539224+baekCode@users.noreply.github.com>
2021-03-14 23:53:14 +0900

    fix : auth router

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -5,6 +5,6 @@ import auth from './auth/index.js'
 const api = new Router();
 
 api.use('/posts', posts.routes());
-api.use('/auth', posts.routes());
+api.use('/auth', auth.routes());
 
 export default api;
