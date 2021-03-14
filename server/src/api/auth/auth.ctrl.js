074aacdaf3e5a85ac30e9ca646eca10634237c70
baekCode <44539224+baekCode@users.noreply.github.com>
2021-03-14 23:28:26 +0900

    feat : auth api register 함수 구현

--- server/src/api/auth/auth.ctrl.js
+++ server/src/api/auth/auth.ctrl.js
@@ -1,28 +1,60 @@
+import Joi from 'joi';
+import User from '../../models/users.js';
 
 /**
  * register 회원가입
+ * POST /api/auth/register
+ * {username: string, password: string}
  * */
 export const register = async ctx => {
+  const schema = Joi.object().keys({
+    username: Joi.string().alphanum().min(3).max(20).required(),
+    password: Joi.string().required()
+  });
+  const result = Joi.validate(ctx.request.body, schema);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
 
-}
+  const {username, password} = ctx.request.body;
+  try {
+    const exists = await User.findByUsername(username);
+    if (exists) {
+      ctx.status = 409;
+      return;
+    }
+
+    const user = new User({username});
+    await user.setPassword(password);
+    await user.save();
+
+    const data = user.toJSON();
+    delete data.hashedPassword;
+    ctx.body = data;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
 /**
  * login 로그인
  * */
 export const login = async ctx => {
 
-}
+};
 
 /**
  * check 로그인 체크상태 확인
  * */
 export const check = async ctx => {
 
-}
+};
 
 /**
  * logout 로그아웃
  * */
 export const logout = async ctx => {
 
-}
+};
