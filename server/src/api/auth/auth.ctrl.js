8c12187354eeb254b5dc8bdfc938aff3bd6bae0b
baekCode <44539224+baekCode@users.noreply.github.com>
2021-03-14 23:07:47 +0900

    feat : auth 컨트롤러 skeleton

---
+++ server/src/api/auth/auth.ctrl.js
@@ -0,0 +1,28 @@
+
+/**
+ * register 회원가입
+ * */
+export const register = async ctx => {
+
+}
+
+/**
+ * login 로그인
+ * */
+export const login = async ctx => {
+
+}
+
+/**
+ * check 로그인 체크상태 확인
+ * */
+export const check = async ctx => {
+
+}
+
+/**
+ * logout 로그아웃
+ * */
+export const logout = async ctx => {
+
+}
