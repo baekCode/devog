38ef3d8b40ef754d668059f01c4d6545df5456d3
baekCode <44539224+baekCode@users.noreply.github.com>
2021-03-15 21:13:13 +0900

    feat : auth 컨트롤러 로그인 구현

--- server/src/api/auth/auth.ctrl.js
+++ server/src/api/auth/auth.ctrl.js
@@ -38,9 +38,37 @@ export const register = async ctx => {
 
 /**
  * login 로그인
+ * POST /api/auth/login
+ * {username: string, password: string}
  * */
 export const login = async ctx => {
+  const {username, password} = ctx.request.body;
 
+  if (!username || !password) {
+    ctx.status = 401;
+    return;
+  }
+
+  try {
+    const user = await User.findByUsername(username);
+
+    if (!user) {
+      ctx.status = 401;
+      return;
+    }
+
+    const valid = await user.checkPassword(password);
+
+    if (!valid) {
+      ctx.status = 401;
+      return;
+    }
+
+    ctx.body = user.serialize();
+
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 /**
