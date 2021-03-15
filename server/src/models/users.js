42add68ba9cf186e065274f868c4b4bce396e4fb
baekCode <44539224+baekCode@users.noreply.github.com>
2021-03-15 21:23:43 +0900

    feat : user model jwt method 구현

--- server/src/models/users.js
+++ server/src/models/users.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const {Schema} = mongoose;
 
@@ -22,6 +23,20 @@ UserSchema.methods.serialize = function () {
   const data = this.toJSON();
   delete data.hashedPassword;
   return data;
+};
+
+UserSchema.methods.generateToken = function () {
+  const token = jwt.sign(
+    {
+      _id     : this.id,
+      username: this.username
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '7d',
+    }
+  );
+  return token;
 };
 
 UserSchema.statics.findByUsername = function (username) {
