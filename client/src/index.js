16d8777948297cdaad4823e7d1c30d6545e04758
baekCode <44539224+baekCode@users.noreply.github.com>
2021-03-15 23:26:10 +0900

    modify : global style 적용

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
+import GlobalStyle from './lib/GlobalStyle';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <BrowserRouter>
+    <GlobalStyle />
     <App />
   </BrowserRouter>,
   document.getElementById('root')
