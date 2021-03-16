86ecf83059d4262a083ddaf7b6b9840629d76577
baekCode <44539224+baekCode@users.noreply.github.com>
2021-03-16 23:17:06 +0900

    feat : redux setting

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import GlobalStyle from './lib/GlobalStyle';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from './module';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <BrowserRouter>
-    <GlobalStyle />
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <GlobalStyle/>
+      <App/>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
