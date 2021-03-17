1e5379ee8806d1336a29a74bf3c215ec5e0c171a
baekCode <44539224+baekCode@users.noreply.github.com>
2021-03-17 22:18:48 +0900

    feat : auth module

--- client/src/module/auth.js
+++ client/src/module/auth.js
@@ -1,14 +1,35 @@
+import produce from 'immer';
 import {createAction, handleActions} from 'redux-actions';
 
-const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
+const CHANGE_FIELD = 'auth/CHANGE_FIELD';
+const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
-export const sampleAction = createAction(SAMPLE_ACTION);
+export const changeFiled = createAction(CHANGE_FIELD, ({form, key, value}) => ({form, key, value}));
+export const initalizeForm = createAction(INITIALIZE_FORM, form => form);
 
-const initialState = {};
+const initialState = {
+  register: {
+    username       : '',
+    password       : '',
+    passwordConfirm: '',
+  },
+  login   : {
+    username: '',
+    password: '',
+  }
+};
 
 const auth = handleActions(
   {
-    [SAMPLE_ACTION]: (state, action) => state,
+    [CHANGE_FIELD]   : (state, {payload: {from, key, value}}) => produce(state, draft => {
+      draft[from][key] = value;
+    }),
+    [INITIALIZE_FORM]: (state, {
+      payload: form
+    }) => ({
+      ...state,
+      [form]: initialState[form]
+    })
   },
   initialState
 );
