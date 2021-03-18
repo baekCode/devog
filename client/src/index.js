31b76379925f7b1abbfd8e3eb052171c63e393bf
baekCode <44539224+baekCode@users.noreply.github.com>
2021-03-18 22:28:10 +0900

    feat : redux saga 미들웨어 적용

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import GlobalStyle from './lib/GlobalStyle';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import rootReducer from './module';
+import rootReducer, {rootSaga} from './module';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
