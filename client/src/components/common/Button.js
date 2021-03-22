446ed15deaef165a5df6d1a8f1e5910132bd8a26
baekCode <44539224+baekCode@users.noreply.github.com>
2021-03-22 23:06:58 +0900

    feat : Button, 링크 추가

--- client/src/components/common/Button.js
+++ client/src/components/common/Button.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
+import {Link} from 'react-router-dom';
 import palette from '../../lib/palette';
 
-const StyledButton = styled.button`
+const buttonStyle = css`
   border: none;
   border-radius: 4px;
   font-size: 1rem;
@@ -16,14 +17,13 @@ const StyledButton = styled.button`
   &:hover {
     background: ${palette.gray[6]};
   }
-  
+
   ${props => props.fullWidth && css`
     width: 100%;
     padding-top: 0.75rem;
     padding-bottom: 0.75rem;
     font-size: 1.125rem;
   `}
-  
   ${props => props.cyan && css`
     background: ${palette.cyan[5]};
 
@@ -33,6 +33,16 @@ const StyledButton = styled.button`
   `}
 `;
 
-const Button = props => <StyledButton {...props} />;
+const StyledButton = styled.button`
+  ${buttonStyle}
+`;
+
+const StyleLink = styled(Link)`
+  ${buttonStyle}
+`;
+
+const Button = props => {
+  return props.to ? <StyleLink {...props} cyan={props.cyan ? 1 : 0}/> : <StyledButton {...props} />;
+};
 
 export default Button;
