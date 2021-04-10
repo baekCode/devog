b772f49ad575a4645e024ad7eed18436d08a9d4c
baekCode <baekcode@gmail.com>
2021-04-11 00:44:46 +0900

    feat : PostItem & Link

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 import Responsive from '../common/Responsive';
+import {Link} from 'react-router-dom';
 
 const Container = styled(Responsive)`
   padding-top: 5rem;
   padding-bottom: 5rem;
 `;
 
+const PostItem = React.memo(({item}) => (<Link to={`/posts/${item._id}`}>{item.title}</Link>));
+
 function Posts({posts}) {
   return (
-    <Container>{posts?.map(item => <p key={item._id}>{item.title}</p>)}</Container>
+    <Container>{posts?.map(item => <PostItem key={item._id} item={item}/>)}</Container>
   );
 }
 
