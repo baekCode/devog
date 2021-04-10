88d8a23e178d5fa1d74538adcbfa3beb29a05d22
baekcode <baekcode@gmail.com>
2021-04-10 15:03:35 +0900

    feat : TagBoxContainer

--- client/src/pages/WritePage.js
+++ client/src/pages/WritePage.js
@@ -3,12 +3,13 @@ import Responsive from '../components/common/Responsive';
 import TagBox from '../components/write/TagBox';
 import ButtonArea from '../components/write/ButtonArea';
 import EditorContainer from '../containers/write/EditorContainer';
+import TagBoxContainer from '../containers/write/TagBoxContainer';
 
 function WritePage(props) {
   return (
     <Responsive>
       <EditorContainer/>
-      <TagBox/>
+      <TagBoxContainer/>
       <ButtonArea/>
     </Responsive>
   );
