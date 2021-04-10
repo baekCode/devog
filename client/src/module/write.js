61f0668ef686c21a930ecf5af7bbddee808723f7
baekcode <baekcode@gmail.com>
2021-04-10 17:45:58 +0900

    feat : write redux saga

--- client/src/module/write.js
+++ client/src/module/write.js
@@ -1,22 +1,48 @@
 import {createAction, handleActions} from 'redux-actions';
+import {takeLatest} from 'redux-saga/effects';
+import createRequestSaga, {createRequestActionTypes} from '../lib/createRequestSaga';
+import * as postsAPI from '../lib/api/post';
 
 const INITIALIZE = 'write/INITIALIZE';
 const CHANGE_FIELD = 'write/CHANGE_FIELD';
+const [WRITE_POST, WRITE_POST_SUCCESS, WRITE_POST_FAILURE] = createRequestActionTypes('write/WRITE_POST');
 
 export const initialize = createAction(INITIALIZE);
 export const changeField = createAction(CHANGE_FIELD, ({key, value}) => ({key, value}));
+export const writePost = createAction(WRITE_POST, ({title, body, tags}) => ({title, body, tags}));
+
+const writePostSaga = createRequestSaga(WRITE_POST, postsAPI.writePost);
+
+export function* writeSaga() {
+  yield takeLatest(WRITE_POST, writePostSaga);
+}
 
 const initailSatate = {
   title: '',
   body : '',
-  tags : []
+  tags : [],
+  post : null,
+  error: null
 };
 
 const write = handleActions({
-  [INITIALIZE]  : state => initailSatate,
-  [CHANGE_FIELD]: (state, {payload: {key, value}}) => ({
+  [INITIALIZE]        : state => initailSatate,
+  [CHANGE_FIELD]      : (state, {payload: {key, value}}) => ({
     ...state,
     [key]: value
+  }),
+  [WRITE_POST]        : state => ({
+    ...state,
+    post : null,
+    error: null,
+  }),
+  [WRITE_POST_SUCCESS]: (state, {payload: post}) => ({
+    ...state,
+    post
+  }),
+  [WRITE_POST_FAILURE]: (state, {payload: error}) => ({
+    ...state,
+    error
   })
 }, initailSatate);
 
--- client/src/pages/WritePage.js
+++ client/src/pages/WritePage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Responsive from '../components/common/Responsive';
-import TagBox from '../components/write/TagBox';
 import ButtonArea from '../components/write/ButtonArea';
 import EditorContainer from '../containers/write/EditorContainer';
 import TagBoxContainer from '../containers/write/TagBoxContainer';
