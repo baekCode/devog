d8235eda4630af9f08896007487eff4cad87ddd7
baekcode <baekcode@gmail.com>
2021-04-10 10:23:01 +0900

    feat : write button area components

--- client/src/pages/WritePage.js
+++ client/src/pages/WritePage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Responsive from '../components/common/Responsive';
 import Editor from '../components/write/Editor';
 import TagBox from '../components/write/TagBox';
+import ButtonArea from '../components/write/ButtonArea';
 
 function WritePage(props) {
   return (
     <Responsive>
       <Editor/>
       <TagBox/>
+      <ButtonArea/>
     </Responsive>
   );
 }
