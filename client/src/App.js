eb9551126647d492d12b455882abb894e60bf1f0
baekCode <44539224+baekCode@users.noreply.github.com>
2021-04-13 23:58:39 +0900

    fix : 라우터시 리다이렉트 되는 오류 쳐리 (Switch, react router dom)

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import {Route, Redirect} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import PostPage from './pages/PostPage';
 import WritePage from './pages/WritePage';
 import LoginPage from './pages/LoginPage';
@@ -7,14 +7,14 @@ import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
-    <>
+    <Switch>
       <Route component={PostListPage} path={['/@:username', '/']} exact/>
       <Route component={PostPage} path='/@:username/:postId'/>
       <Route component={LoginPage} path='/login'/>
       <Route component={RegisterPage} path='/register'/>
       <Route component={WritePage} path='/write'/>
       <Redirect from={'*'} to={'/'}/>
-    </>
+    </Switch>
   );
 }
 
