0be4862670adc36b8f7c6dbf511b66f7fc3f6f39
baekCode <44539224+baekCode@users.noreply.github.com>
2021-04-13 23:57:37 +0900

    fix : merge 충돌 해결

--- client/src/pages/WritePage.js
+++ client/src/pages/WritePage.js
@@ -1,28 +1,18 @@
 import React from 'react';
-<<<<<<< HEAD
-import Editor from '../components/write/Editor';
-import HeaderContainer from '../containers/common/HeaderContainer';
-
-function WritePage(props) {
-  return (
-    <>
-      <HeaderContainer/>
-      <Editor/>
-    </>
-=======
 import Responsive from '../components/common/Responsive';
 import EditorContainer from '../containers/write/EditorContainer';
 import TagBoxContainer from '../containers/write/TagBoxContainer';
 import ButtonContainer from '../containers/write/ButtonContainer';
+import HeaderContainer from '../containers/common/HeaderContainer';
 
 function WritePage(props) {
   return (
     <Responsive>
+      <HeaderContainer/>
       <EditorContainer/>
       <TagBoxContainer/>
       <ButtonContainer/>
     </Responsive>
->>>>>>> b772f49ad575a4645e024ad7eed18436d08a9d4c
   );
 }
 
