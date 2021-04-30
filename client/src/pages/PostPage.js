c05724cf24980195be48b54401a98c2c2db3c9f8
baekCode <baekcode@gmail.com>
2021-05-01 08:51:02 +0900

    modify : PostContainer

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PostViewer from '../components/post/PostViewer';
 import HeaderContainer from '../containers/common/HeaderContainer';
+import PostContainer from '../containers/post/postContainer';
 
 function PostPage(props) {
   return (
     <>
       <HeaderContainer/>
-      <PostViewer/>
+      <PostContainer/>
     </>
   );
 }
