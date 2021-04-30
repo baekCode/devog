56cbe0d3d99a7d2e1b3a1e344679f95fe3b44b1e
baekCode <baekcode@gmail.com>
2021-05-01 08:50:16 +0900

    feat : post api

--- client/src/lib/api/posts.js
+++ client/src/lib/api/posts.js
@@ -2,3 +2,4 @@ import client from './client';
 
 export const writePost = ({title, body, tags}) => client.post('/api/posts', {title, body, tags});
 export const getPosts = () => client.get('/api/posts');
+export const getPost = id => client.get(`/api/posts/${id}`);
