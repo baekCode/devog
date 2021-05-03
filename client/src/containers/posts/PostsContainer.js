3f6b8e5744750b5af19cc1ffc4e3a3bad1f7a9e6
baekCode <baekcode@gmail.com>
2021-05-03 23:22:13 +0900

    feat : posts container

--- client/src/containers/posts/PostsContainer.js
+++ client/src/containers/posts/PostsContainer.js
@@ -1,19 +1,26 @@
+import * as qs from 'qs';
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
+import {withRouter} from 'react-router-dom';
 import {getPosts} from '../../module/posts';
 import Posts from '../../components/posts/Posts';
 
-function PostsContainer(props) {
+function PostListContainer({location}) {
   const dispatch = useDispatch();
-  const {posts} = useSelector(({posts}) => ({
-    posts: posts.posts
+  const {posts, error, loading, user} = useSelector(({posts, loading, user}) => ({
+    posts  : posts.posts,
+    error  : posts.error,
+    loading: loading['posts/POSTS'],
+    user   : user.user
   }));
 
   useEffect(() => {
-    dispatch(getPosts());
-  }, [dispatch]);
-
-  return <Posts posts={posts}/>;
+    const {tag, username, page} = qs.parse(location.search, {ignoreQueryPrefix: true});
+    dispatch(getPosts({tag, username, page}));
+  }, [dispatch, location.search]);
+  return (
+    <Posts loading={loading} error={error} posts={posts} showWriteButton={user}/>
+  );
 }
 
-export default PostsContainer;
+export default withRouter(PostListContainer);
