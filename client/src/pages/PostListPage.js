9792e6874a0a203b9e4564058e5465d4148c1130
baekCode <baekcode@gmail.com>
2021-05-05 09:11:07 +0900

    feat : pagination container 추가

--- client/src/pages/PostListPage.js
+++ client/src/pages/PostListPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeaderContainer from '../containers/common/HeaderContainer';
 import PostsContainer from '../containers/posts/PostsContainer';
+import PaginationContainer from '../containers/posts/PaginationContainer';
 
 function PostListPage(props) {
   return (
     <>
       <HeaderContainer/>
       <PostsContainer/>
+      <PaginationContainer/>
     </>
   );
 }
