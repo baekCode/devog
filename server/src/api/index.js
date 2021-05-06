6f9126e54d0c51c05d10461994ffc14ed2660910
baekCode <baekcode@gmail.com>
2021-05-06 21:53:17 +0900

    feat : about api

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,10 +1,12 @@
 import Router from 'koa-router';
 import posts from './posts/index.js';
-import auth from './auth/index.js'
+import auth from './auth/index.js';
+import about from './about/index.js';
 
 const api = new Router();
 
 api.use('/posts', posts.routes());
 api.use('/auth', auth.routes());
+api.use('/about', about.routes());
 
 export default api;
